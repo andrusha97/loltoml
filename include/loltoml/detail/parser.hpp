650885d218b2185ea9d045936d607543f7225f7e
Andrey Goryachev <andrey.goryachev@gmail.com>
2016-07-26 23:56:22 +0300

    Fix parsing of hex-digits.

--- include/loltoml/detail/parser.hpp
+++ include/loltoml/detail/parser.hpp
@@ -400,9 +400,9 @@ private:
 
         if (ch >= '0' && ch <= '9') {
             return ch - '0';
-        } else if (ch >= 'A' && ch <= 'Z') {
+        } else if (ch >= 'A' && ch <= 'F') {
             return ch - 'A' + 10;
-        } else if (ch >= 'a' && ch <= 'z') {
+        } else if (ch >= 'a' && ch <= 'f') {
             return ch - 'a' + 10;
         } else {
             throw parser_error_t("Expected hex-digit", last_char_offset());
